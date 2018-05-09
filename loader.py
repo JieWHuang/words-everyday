dfb4ec2f5b7fd6c4e496ef7b7c98e75ec256f90e
mickys <huangyz0918@gmail.com>
2018-05-09 19:57:12 +0800

    ✨ feat: finished all, waitting for tests

--- loader.py
+++ loader.py
@@ -4,12 +4,12 @@ import sys
 import xlrd
 import time
 import os
-import pandas as pd
+import xlwt
 
 # open the xls file
 
 
-def open_excel(file='words-book/xls/fojiao.xlsx'):
+def open_excel(file='words-book/xls/fojiao.xls'):
     try:
         data = xlrd.open_workbook(file)
         return data
@@ -21,8 +21,7 @@ def open_excel(file='words-book/xls/fojiao.xlsx'):
 
 def read_excel():
     data = open_excel()
-    # tableOrdered = data.sheets()[0]
-    tableDisordered = data.sheets()[4]
+    tableDisordered = data.sheets()[0]
     nrows = tableDisordered.nrows
     colnames = tableDisordered.row_values(0)
     list = []
@@ -39,7 +38,7 @@ def read_excel():
 # You can set how many words you want to recite everyday
 
 
-def write_words(tables, wordNum=100):
+def write_words(tables, wordNum=10):
     script_dir = os.path.dirname(os.path.realpath('__file__'))
     today = time.strftime("%Y-%m-%d")
     todayFileName = today + ".md"
@@ -48,15 +47,32 @@ def write_words(tables, wordNum=100):
     todayFile = open(today_file_path, 'a', encoding='utf-8')
     # print (today_file_path)
     count = 0
-    while count < 10:
+    while count < wordNum:
         row = tables[count]
         word = row[0]
         characteristic = row[1]
         discription = row[2]
-        # todayFile.write('| %s | %s | %s |\n' % (word, characteristic, discription))
+        todayFile.write('| %s | %s | %s |\n' % (word, characteristic, discription))
         print (word,characteristic,discription)
         count = count + 1
         pass
+
+    print (len(tables))
+    count_rest = count;
+    wbk = xlwt.Workbook()
+    sheet = wbk.add_sheet('disordered')
+    while count_rest < len(tables):
+        print (count_rest)
+        row = tables[count_rest]
+        word = row[0]
+        characteristic = row[1]
+        discription = row[2]
+        sheet.write(count_rest - wordNum, 0, word)
+        sheet.write(count_rest - wordNum, 1, characteristic)
+        sheet.write(count_rest - wordNum, 2, discription)
+        count_rest = count_rest + 1
+        pass
+    wbk.save('words-book/xls/fojiao.xls')
 
     # writer = pd.ExcelWriter('words-book/xls/fojiao.xlsx')
 
@@ -68,17 +84,13 @@ def write_words(tables, wordNum=100):
     #     print (word,characteristic,discription)
 
 def tests():
-    data = pd.read_excel('words-book/xls/fojiao.xlsx', sheet_name="disordered")
-    data.drop(data.index[[2515,2516]],inplace=True)
+
     return
 
 def main():
-    # tables = read_excel()
-    # write_words(tables)
-    tests()
-
-
-
+    tables = read_excel()
+    write_words(tables)
+    # tests()
 
 if __name__ == "__main__":
     main()
